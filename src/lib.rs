91cd98457417d4477d22afd66a5485ae29796e10
jyli <jyli@dtu.dk>
2022-01-05 17:36:55 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,108 @@
+use num::traits::{Float, FromPrimitive, ToPrimitive};
+
+pub fn abs_difference<T: std::ops::Sub<Output = T> + std::cmp::PartialOrd>(x: T, y: T) -> T {
+    if x < y {
+        y - x
+    } else {
+        x - y
+    }
+}
+
+#[derive(Debug)]
+enum Zone {
+    Begin,
+    Uphill,
+    Downhill,
+}
+
+pub fn get_peaktrough<T>(signal: &Vec<T>, thres: T) -> Vec<T>
+where
+    T: Float + FromPrimitive + ToPrimitive + std::fmt::Debug,
+{
+    let mut peaktrough: Vec<T> = Vec::new();
+    let mut zone = Zone::Begin;
+    let mut peak = signal[0];
+    let mut trough = signal[0];
+
+    for x in signal.iter() {
+        match zone {
+            Zone::Begin if *x > peak => {
+                peak = *x;
+                if peak - trough > thres {
+                    peaktrough.push(trough);
+                    zone = Zone::Uphill;
+                }
+            }
+            Zone::Begin if *x < trough => {
+                trough = *x;
+                if peak - trough > thres {
+                    peaktrough.push(peak);
+                    zone = Zone::Downhill;
+                }
+            }
+            Zone::Uphill if *x > peak => peak = *x,
+            Zone::Uphill if peak - *x >= thres => {
+                peaktrough.push(peak);
+                trough = *x;
+                zone = Zone::Downhill;
+            }
+            Zone::Downhill if *x < trough => trough = *x,
+            Zone::Downhill if *x - trough >= thres => {
+                peaktrough.push(trough);
+                peak = *x;
+                zone = Zone::Uphill;
+            }
+            _ => (),
+        }
+    }
+    peaktrough.push(match zone {
+        Zone::Uphill => peak,
+        Zone::Downhill => trough,
+        Zone::Begin => (peak + trough) / T::from(2).unwrap(),
+    });
+    peaktrough
+}
+
+pub fn get_amplitudes<T>(peaktrough: &Vec<T>) -> Vec<T>
+where
+    T: Float + FromPrimitive + ToPrimitive + std::fmt::Debug,
+{
+    let mut amplitudes: Vec<T> = Vec::new();
+    let mut S = peaktrough.clone();
+    let mut i: usize = 3;
+
+    // phase 1
+    while i < S.len() {
+        if (S[i - 2] > S[i - 3] && S[i - 1] >= S[i - 3] && S[i] >= S[i - 2])
+            || (S[i - 2] < S[i - 3] && S[i - 1] <= S[i - 3] && S[i] <= S[i - 2])
+        {
+            amplitudes.push(abs_difference(S[i - 2], S[i - 1]));
+            amplitudes.push(abs_difference(S[i - 2], S[i - 1]));
+            S.remove(i - 1);
+            S.remove(i - 2);
+        } else {
+            i += 1;
+        }
+    }
+    // phase 2
+    for (i, s) in S[0..S.len() - 1].iter().enumerate() {
+        amplitudes.push(abs_difference(S[i + 1], *s));
+    }
+
+    amplitudes
+}
+
+pub fn eq_load<T>(signal: &Vec<T>, m: f64, neq: u64) -> f64
+where
+    T: Float + FromPrimitive + ToPrimitive + std::fmt::Debug,
+{
+    let peaktrough = get_peaktrough(&signal, T::from(0).unwrap());
+    let amplitudes = get_amplitudes(&peaktrough);
+
+    let sum_damage: f64 = amplitudes
+        .iter()
+        .map(|x| T::to_f64(x).unwrap().powf(m))
+        .sum();
+    let del = (sum_damage / (neq as f64)).powf(1.0 / m);
+    del
+}
---
+++ tests/tests.rs
@@ -0,0 +1,48 @@
+use rustfatigue::{abs_difference, eq_load, get_amplitudes, get_peaktrough};
+
+static INP: [f64; 20] = [
+    40.0, 15.0, 72.0, 22.0, 43.0, 82.0, 75.0, 7.0, 34.0, 49.0, 95.0, 75.0, 85.0, 47.0, 63.0, 31.0,
+    90.0, 20.0, 37.0, 39.0,
+];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn peaktrough_no_threshold() {
+        let ans: [f64; 15] = [
+            40.0, 15.0, 72.0, 22.0, 82.0, 7.0, 95.0, 75.0, 85.0, 47.0, 63.0, 31.0, 90.0, 20.0, 39.0,
+        ];
+        assert!(get_peaktrough(&INP.to_vec(), 0.0)
+            .iter()
+            .enumerate()
+            .all(|(i, &val)| val == ans[i]));
+    }
+    #[test]
+    fn peaktrough_with_threshold() {
+        let ans: [f64; 10] = [40.0, 15.0, 72.0, 22.0, 82.0, 7.0, 95.0, 31.0, 90.0, 20.0];
+
+        assert!(get_peaktrough(&INP.to_vec(), 20.0)
+            .iter()
+            .enumerate()
+            .all(|(i, &val)| val == ans[i]));
+    }
+    #[test]
+    fn amptest() {
+        let ans: [f64; 14] = [
+            50.0, 50.0, 10.0, 10.0, 16.0, 16.0, 59.0, 59.0, 25.0, 67.0, 75.0, 88.0, 75.0, 19.0,
+        ];
+        let peaktroughs = get_peaktrough(&INP.to_vec(), 0.0);
+        assert!(get_amplitudes(&peaktroughs)
+            .iter()
+            .enumerate()
+            .all(|(i, &val)| val == ans[i]));
+    }
+
+    #[test]
+    fn eq_load_float() {
+        let out = eq_load(&INP.to_vec(), 4.0, 20);
+        println!("{}", out);
+        assert!(abs_difference(out, 54.83366824817066) < 0.0001);
+    }
+}
