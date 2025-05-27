42a969db674cc03c137fc57ec6c54b04cac192e1
Jaime Liew <jaimeliew1@gmail.com>
2025-05-27 21:00:08 +0200

    rename function. add doc string and typing.

--- rustfatigue/rustinterface.py
+++ rustfatigue/rustinterface.py
@@ -2,7 +2,24 @@ import numpy as np
 from . import rustfatigue
 
 
-def eq_load(signal, m, neq, half=True):
+def damage_equiv_load(signal: list[float], m: float, neq: int, half: bool = True):
+    """
+    Calculate the Damage Equivalent Load (DEL) for a given signal.
+
+    Parameters:
+        signal (array-like): The time-series load signal.
+        m (float): Wöhler exponent.
+        neq (int): Number of equivalent load cycles.
+        half (bool): Whether to count the residual cycles as half (True) or
+                     whole (False) cycles. (default=True).
+
+    Returns:
+        float: The damage equivalent load.
+    """
     return rustfatigue.eq_load_python(
         np.array(signal, dtype=np.float64), float(m), int(neq), half
     )
+
+
+# Backward-compatible alias
+eq_load = damage_equiv_load
